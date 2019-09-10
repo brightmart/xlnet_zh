7d34882fbdc3ecc65f1916e243d59100fdb57da0
b <brightmart@hotmail.com>
2019-09-10 22:00:16 +0800

    update

--- src/data_utils.py
+++ src/data_utils.py
@@ -69,8 +69,11 @@ def format_filename(prefix, bsz_per_host, seq_len, bi_data, suffix,
   else:
     fnp_str = ""
 
-  file_name = "{}.bsz-{}.seqlen-{}.{}{}{}.alpha-{}.beta-{}.{}{}".format(
-      prefix, bsz_per_host, seq_len, reuse_len_str, uncased_str, bi_data_str,
+  #file_name = "{}.bsz-{}.seqlen-{}.{}{}{}.alpha-{}.beta-{}.{}{}".format(
+  #    prefix, bsz_per_host, seq_len, reuse_len_str, uncased_str, bi_data_str,
+  #    mask_alpha, mask_beta, fnp_str, suffix)
+  file_name = "{}.seqlen-{}.{}{}{}.alpha-{}.beta-{}.{}{}".format(
+      prefix, seq_len, reuse_len_str, uncased_str, bi_data_str,
       mask_alpha, mask_beta, fnp_str, suffix)
   print("#####file_name:",file_name)
 
